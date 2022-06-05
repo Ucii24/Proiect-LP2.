50712cd0e3a5f93d84899f01093c1fc6776996a7
Iova Laurean <106405056+IovaLaurean@users.noreply.github.com>
2022-06-05 18:46:37 +0300

    Add files via upload

---
+++ Interfata clanedar.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkcalendar import Calendar
+# Se creaza interfata
+root = Tk()
+
+# Setam Dimensiunea interfetei
+root.geometry("300x400")
+
+# Adaugam Calendarul
+cal = Calendar(root, selectmode = 'day',
+               year = 2022, month = 5,
+               day = 25)
+
+cal.pack(pady = 20)
+
+def grad_date():
+    date.config(text = "Data selectata este: " + cal.get_date())
+
+# Adaugare buton + eticheta
+Button(root, text = "Alege o data",
+       command = grad_date).pack(pady = 20)
+
+date = Label(root, text = "")
+date.pack(pady = 20)
+
+# Executam libraria Tkinter
+root.mainloop()
+
